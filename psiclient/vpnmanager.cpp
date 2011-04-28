0110590551865a3cfa9abbaf2f3f4327f0e3aea1
Adam Kruger <a.kruger@psiphon.ca>
2011-04-28 14:21:11 -0400

    Add Initializing state to prevent calling TryNextServer (and Establish) while a TryNextServer or Establish is already outstanding.

--- psiclient/vpnmanager.cpp
+++ psiclient/vpnmanager.cpp
@@ -35,28 +35,41 @@ VPNManager::~VPNManager(void)
 
 void VPNManager::Toggle()
 {
-    if (VPN_STATE_STOPPED == m_vpnState)
+    switch (m_vpnState)
     {
+    case VPN_STATE_STOPPED:
         // The user clicked the button to start the VPN.
         // Clear this flag so we can do retries on failed connections.
         m_userSignalledStop = false;
         TryNextServer();
-    }
-    else
-    {
+        break;
+
+    default:
         // The user requested to stop the VPN by clicking the button.
-        // If a connection was in the "Establishing" state, we will get a
+        //
+        // If a connection was in the INITIALIZING state, this flag
+        // tells TryNextServer not to Establish, or to Stop if
+        // Establish was already called.
+        // NOTE that Stop is called here in case TryNextServer has
+        // already returned (and the first callback notification has
+        // not yet been received).
+        //
+        // If a connection was in the STARTING state, we will get a
         // "Connection Failed" notification.
         // This flag indicates that we should not retry when a failed
         // connection is signalled.
         m_userSignalledStop = true;
         Stop();
+        break;
     }
 }
 
 void VPNManager::Stop(void)
 {
-    m_vpnConnection.Remove();
+    if (m_vpnConnection.Remove())
+    {
+        VPNStateChanged(VPN_STATE_STOPPED);
+    }
 }
 
 void VPNManager::VPNStateChanged(VPNState newState)
@@ -92,6 +105,8 @@ void VPNManager::VPNStateChanged(VPNState newState)
 
 void VPNManager::TryNextServer(void)
 {
+    VPNStateChanged(VPN_STATE_INITIALIZING);
+
     // Try the next server in our list.
     ServerEntry serverEntry = m_vpnList.GetNextServer();
 
@@ -104,5 +119,17 @@ void VPNManager::TryNextServer(void)
     // TODO: do web request to get the PSK
     // TODO: if the web request is synchronous, do not continuously retry because
     //       that will prevent us from processing another toggle click.
-    m_vpnConnection.Establish(serverAddress, _T("1q2w3e4r!"));
+    // NOTE: Toggle may have been clicked since the start of this function.
+    //       If it was, don't Establish the VPN connection.
+    if (!m_userSignalledStop)
+    {
+        m_vpnConnection.Establish(serverAddress, _T("1q2w3e4r!"));
+    }
+
+    // NOTE: Toggle may have been clicked during Establish.
+    //       If it was, Stop the VPN.
+    if (m_userSignalledStop)
+    {
+        Stop();
+    }
 }
