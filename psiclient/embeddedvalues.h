ba9a5f3f379e3b8a1d17ee789fb1aedf549315f5
Rod Hynes <r.hynes@psiphon.ca>
2011-05-03 15:29:55 -0400

    Server: Added web server (integrated with spreadsheet); Client: integrated HTTP request with VPN Manager

---
+++ psiclient/embeddedvalues.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2011, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#pragma once
+
+static const char* CLIENT_ID = 
+    "B0252ED1E54F9466";
+
+static const char* PSIPHON_CA_CERT =
+    "B4FDCA80A6C01F6B5B2712BCDFD56FB890E2A0CDA58BE9E36788D45BFAB69FBE"
+    "FF51CBA580FF7B5600F1D163B52CC4201B115B82DCDED5AD389E94E677590F03"
+    "A45B239D99A2418F8DD4E32AAFE780B03696249D21339C6E50202CCAF38887B1"
+    "8C09A857D20BF284531677264F33B30D0A83F6DCAA4663620DF80ED445A0DA82"
+    "C5F4BC6A4335473F0D37B0252ED1E54F9466D6D8EAB23A3970C4DB15053947E8"
+    "3627AE9F649776818692052E14CCD75DC8275A915C8C78B523307BCE7684D2FC"
+    "2E3C285C5A7C3D6A0EB8FD754D5B6D913BEADEAE254147F752391F052BFB4B61"
+    "5CEAC356123C4C3AD8B00169F08397EBEE6D96AC0E42748F853C97D427901815";
+
+static const char* EMBEDDED_SERVER_LIST =
+    "31302e302e302e32302038302046383934463333333930354638333137\n"
+    "3139322e3136382e302e32302038302038384546324644343332343732353542\n"
+    "3137322e31362e302e32302038302033464533344238393132423838354242\n";
---
+++ psiclient/httpsrequest.cpp
@@ -0,0 +1,254 @@
+/*
+ * Copyright (c) 2011, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "stdafx.h"
+#include "httpsrequest.h"
+#include "psiclient.h"
+#include <Winhttp.h>
+#include <WinCrypt.h>
+
+HTTPSRequest::HTTPSRequest(void)
+{
+}
+
+HTTPSRequest::~HTTPSRequest(void)
+{
+}
+
+bool HTTPSRequest::GetRequest(
+        bool& cancel,
+        const TCHAR* serverAddress,
+        int serverWebPort,
+        const TCHAR* requestPath,
+        string& response)
+{
+    // TODO:
+    // Use asynchronous mode for cleaner and more effectively cancel functionality:
+    // http://msdn.microsoft.com/en-us/magazine/cc716528.aspx
+    // http://msdn.microsoft.com/en-us/library/aa383138%28v=vs.85%29.aspx
+    // http://msdn.microsoft.com/en-us/library/aa384115%28v=VS.85%29.aspx
+
+    BOOL bRet = FALSE;
+    CERT_CONTEXT *pCert = {0};
+    HCERTSTORE hCertStore = NULL;
+    DWORD dwRet = 0;
+    DWORD dwLen = 0;
+    DWORD dwStatusCode;
+    DWORD dwFlags = SECURITY_FLAG_IGNORE_CERT_CN_INVALID |
+				    SECURITY_FLAG_IGNORE_CERT_DATE_INVALID |
+				    SECURITY_FLAG_IGNORE_UNKNOWN_CA;
+    LPVOID pBuffer = NULL;
+
+    AutoHINTERNET hSession =
+                WinHttpOpen(
+                    _T("Mozilla/4.0 (compatible; MSIE 5.22)"),
+	                WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
+	                WINHTTP_NO_PROXY_NAME,
+	                WINHTTP_NO_PROXY_BYPASS,
+                    0 );
+
+    if (NULL == hSession)
+    {
+	    my_print(false, _T("WinHttpOpen failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    AutoHINTERNET hConnect =
+            WinHttpConnect(
+                hSession,
+	            serverAddress,
+	            serverWebPort,
+	            0);
+
+    if (NULL == hConnect)
+    {
+	    my_print(false, _T("WinHttpConnect failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    AutoHINTERNET hRequest =
+            WinHttpOpenRequest(
+                    hConnect,
+	                _T("GET"),
+	                requestPath,
+	                NULL,
+	                WINHTTP_NO_REFERER,
+	                WINHTTP_DEFAULT_ACCEPT_TYPES,
+	                WINHTTP_FLAG_SECURE); 
+
+    if (NULL == hRequest)
+    {
+	    my_print(false, _T("WinHttpOpenRequest failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    bRet = WinHttpSetOption(
+	            hRequest,
+	            WINHTTP_OPTION_SECURITY_FLAGS,
+	            &dwFlags,
+	            sizeof(DWORD));
+
+    if (FALSE == bRet)
+    {
+	    my_print(false, _T("WinHttpSetOption failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    bRet = WinHttpSendRequest(
+                hRequest,
+	            WINHTTP_NO_ADDITIONAL_HEADERS,
+	            0,
+	            WINHTTP_NO_REQUEST_DATA,
+	            0,
+	            0,
+	            0);
+
+    if (FALSE == bRet)
+    {
+	    my_print(false, _T("WinHttpSendRequest failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    bRet = WinHttpReceiveResponse(hRequest, NULL);
+
+    if (FALSE == bRet)
+    {
+	    my_print(false, _T("WinHttpReceiveResponse failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    dwLen = sizeof(dwStatusCode);
+
+    bRet = WinHttpQueryHeaders(
+                    hRequest, 
+                    WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
+                    NULL, 
+                    &dwStatusCode, 
+                    &dwLen, 
+                    NULL);
+
+    if (FALSE == bRet)
+    {
+	    my_print(false, _T("WinHttpQueryHeaders failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (200 != dwStatusCode)
+    {
+	    my_print(false, _T("Bad HTTP GET request status code: %d"), dwStatusCode);
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    bRet = WinHttpQueryDataAvailable(hRequest, &dwLen);
+
+    if (FALSE == bRet)
+    {
+	    my_print(false, _T("WinHttpQueryDataAvailable failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    pBuffer = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwLen);
+
+    if (!pBuffer)
+    {
+        my_print(false, _T("HeapAlloc failed"));
+        return false;
+    }
+    
+    bRet = WinHttpReadData(hRequest, pBuffer, dwLen, &dwLen);
+
+    if (FALSE == bRet)
+    {
+	    my_print(false, _T("WinHttpReadData failed (%d)"), GetLastError());
+        HeapFree(GetProcessHeap(), 0, pBuffer);
+        return false;
+    }
+
+    my_print(false, _T("got: %s"), string((const char*)pBuffer, dwLen).c_str());
+
+    HeapFree(GetProcessHeap(), 0, pBuffer);
+
+    dwLen = sizeof(pCert);
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    bRet = WinHttpQueryOption(
+	            hRequest,
+	            WINHTTP_OPTION_SERVER_CERT_CONTEXT,
+	            &pCert,
+	            &dwLen);
+
+    if (NULL == pCert)
+    {
+	    my_print(false, _T("WinHttpQueryOption failed (%d)"), GetLastError());
+        return false;
+    }
+
+    if (cancel)
+    {
+        return false;
+    }
+
+    my_print(false, _T("cert encoding %d %d"), pCert->dwCertEncodingType, pCert->cbCertEncoded);
+
+    return true;
+}
