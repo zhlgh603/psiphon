2cfcaee4efc5502c1a0ae6c819a966b19d72ffd4
Adam Kruger <a.kruger@psiphon.ca>
2011-05-26 16:23:42 -0400

    Add vpn_client_ip_address to the connected https request.

---
+++ Automation/psi_deploy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2011, Psiphon Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# TODO: script to deploy latest configuration to all servers
+
+# for each server in db:
+#   if missing cert, generate and write to db
+#   subset_db = db subset with only info required by server
+#   ssh to server and put sub_db
+#   also copy all required client builds
