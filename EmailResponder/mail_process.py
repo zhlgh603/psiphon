dbc69850df854373b59c54f149156a5e634bb385
apritchard <a.pritchard@psiphon.ca>
2011-11-10 13:52:19 -0500

    Added function to create report from stats. Checking it in before I tear it out and start again.

---
+++ EmailResponder/mail_process.py
@@ -0,0 +1,258 @@
+# Copyright (c) 2011, Psiphon Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import sys
+import os
+import syslog
+import email
+import email.header
+import json
+import re
+import traceback
+import time
+import tempfile
+
+import settings
+import sendmail
+import blacklist
+import mail_stats
+
+
+
+def strip_email(email_address):
+    '''
+    Strips something that looks like:
+        Fname Lname <mail@example.com>
+    Down to just mail@example.com and returns it. If passed a plain email address, 
+    will return that email. Returns False if bad email address.
+    '''
+
+    # This regex is adapted from:
+    # https://gitweb.torproject.org/gettor.git/blob/HEAD:/lib/gettor/requests.py
+    to_regex = '.*?(<)?([a-zA-Z0-9\+\.\-]+@[a-zA-Z0-9\+\.\-]+\.[a-zA-Z0-9\+\.\-]+)(?(1)>).*'
+    match = re.match(to_regex, email_address)
+    if match and match.group(2):
+        return match.group(2)
+    return False
+    
+
+def decode_header(header_val):
+    '''
+    Returns False if decoding fails. Otherwise returns the decoded value.
+    '''
+    try:
+        hdr = email.header.decode_header(header_val)
+        if not hdr: return False
+        text, encoding = hdr[0]
+        if not encoding: return text
+        return text.decode(encoding)        
+    except Exception:
+        return False
+
+
+class MailResponder:
+    '''
+    Takes a configuration file and an email and sends back the appropriate 
+    response to the sender.
+    '''
+
+    def __init__(self):
+        self.requested_addr = None
+        self._conf = None
+        self._email = None
+        self.stats = mail_stats.MailStats()
+
+    def read_conf(self, conf_filepath):
+        '''
+        Reads in the given configuration file.
+        Return True if successful, False otherwise.
+        '''
+
+        try:
+            conffile = open(conf_filepath, 'r')
+
+            self._response_from_addr = settings.RESPONSE_FROM_ADDR
+
+            # Note that json.load reads in unicode strings
+            self._conf = json.load(conffile)
+            
+        except Exception as e:
+            syslog.syslog(syslog.LOG_CRIT, 'Failed to read conf file: %s' % e)
+            return False
+
+        return True
+
+    def process_email(self, email_string):
+        '''
+        Processes the given email and sends a response.
+        Returns True if successful, False or exception otherwise.
+        '''
+
+        if not self._parse_email(email_string):
+            return False
+        
+        # Is this a verification email from Amazon SES?
+        if self._check_verification_email():
+            return False
+
+        # Look up requested email address. 
+        if not self._conf.has_key(self.requested_addr):
+            syslog.syslog(syslog.LOG_INFO, 'recip_addr invalid: %s' % self.requested_addr)
+            
+            # Record if this email was sent to our no-reply address
+            if self.requested_addr == strip_email(settings.RESPONSE_FROM_ADDR):
+                self.stats.increment_stats_noreply()
+                
+            return False
+        
+        # Check if the user is (or should be) blacklisted
+        if not self._check_blacklist():
+            syslog.syslog(syslog.LOG_INFO, 'requester blacklisted')
+            self.stats.increment_stats_blacklist()
+            return False
+
+        raw_response = sendmail.create_raw_email(self._requester_addr, 
+                                                 self._response_from_addr,
+                                                 self._subject,
+                                                 self._conf[self.requested_addr])
+
+        if not raw_response:
+            return False
+
+        if not sendmail.send_raw_email_amazonses(raw_response, 
+                                                 self._response_from_addr):
+            return False
+
+        # Record the stats for the successful response
+        self.stats.increment_stats_per_addr(self.requested_addr)
+
+        return True
+
+    def _check_blacklist(self):
+        bl = blacklist.Blacklist()
+        return bl.check_and_add(strip_email(self._requester_addr))
+
+    def _parse_email(self, email_string):
+        '''
+        Extracts the relevant items from the email.
+        '''
+
+        self._email = email.message_from_string(email_string)
+
+        self.requested_addr = decode_header(self._email['To'])
+        if not self.requested_addr:
+            syslog.syslog(syslog.LOG_INFO, 'No recip_addr')
+            return False
+        
+        # The 'To' field generally looks like this: 
+        #    "get+fa" <get+fa@psiphon3.com>
+        # So we need to strip it down to the useful part.
+
+        self.requested_addr = strip_email(self.requested_addr)
+        if not self.requested_addr:
+            # Bad address. Fail.
+            syslog.syslog(syslog.LOG_INFO, 'Unparsable requested_addr')
+            return False
+
+        # Convert to lowercase, since that's what's in the _conf and we want to 
+        # do a case-insensitive check.
+        self.requested_addr = self.requested_addr.lower()
+
+        self._requester_addr = decode_header(self._email['Return-Path'])
+        if not self._requester_addr:
+            syslog.syslog(syslog.LOG_INFO, 'No _requester_addr')
+            return False
+
+        self._subject = decode_header(self._email['Subject'])
+        if not self._subject: self._subject = '' 
+
+        # Add 'Re:' to the subject
+        self._subject = u'Re: %s' % self._subject
+
+        return True
+    
+    def _check_verification_email(self):
+        '''
+        Check if the incoming email is an Amazon SES verification email that
+        we should write to file so that we use the link in it.
+        '''
+        if settings.VERIFY_EMAIL_ADDRESS \
+           and settings.VERIFY_EMAIL_ADDRESS == self.requested_addr:
+            
+            # Write the email to disk so that we can get the verification link 
+            # out of it.
+            f = open(settings.VERIFY_FILENAME, 'w')
+            f.write(self._email.as_string())
+            f.close()
+            
+            syslog.syslog(syslog.LOG_INFO, 'verification email received to: %s' % self.requested_addr)
+            
+            return True
+        
+        return False
+        
+
+
+
+if __name__ == '__main__':
+    '''
+    Note that we always exit with 0 so that the email server doesn't complain.
+    '''
+    
+    starttime = time.clock()
+
+    if len(sys.argv) < 2:
+        raise Exception('Not enough arguments. conf file required')
+
+    conf_filename = sys.argv[1]
+
+    if not os.path.isfile(conf_filename):
+        raise Exception('conf file must exist: %s' % conf_filename)
+
+    try:
+        email_string = sys.stdin.read()
+
+        if not email_string:
+            syslog.syslog(syslog.LOG_CRIT, 'No stdin')
+            exit(0)
+
+        responder = MailResponder()
+
+        if not responder.read_conf(conf_filename):
+            exit(0)
+
+        if not responder.process_email(email_string):
+            exit(0)
+
+    except Exception as e:
+        syslog.syslog(syslog.LOG_CRIT, 'Exception caught: %s' % e)
+        syslog.syslog(syslog.LOG_CRIT, traceback.format_exc())
+        responder.stats.increment_stats_exceptions()
+        
+        # Should we write this exception-causing email to disk?
+        if settings.EXCEPTION_DIR and email_string:
+            temp = tempfile.mkstemp(suffix='.txt', dir=settings.EXCEPTION_DIR)
+            tempfile = os.fdopen(temp[0], 'w')
+            tempfile.write('Exception caught: %s\n' % e)
+            tempfile.write('%s\n\n' % traceback.format_exc())
+            tempfile.write(email_string)
+            tempfile.close()
+    else:
+        syslog.syslog(syslog.LOG_INFO, 
+                      'Responded successfully to request for: %s: %fs' % (responder.requested_addr, time.clock()-starttime))
+    
+    exit(0)
---
+++ EmailResponder/mail_stats.py
@@ -0,0 +1,161 @@
+# Copyright (c) 2011, Psiphon Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+'''
+Intended to be run as a cron job to send SES quota and stats to desired email
+address (using SES itself). Credentials for SES are expected to be in boto 
+conf file or environment variables.
+
+The MailStats class can be used to record stats about the mail responder.
+'''
+
+import json
+import argparse
+import MySQLdb as mdb
+from boto.ses.connection import SESConnection
+import settings
+import sendmail
+
+
+class MailStats(object):
+    '''
+    Writes new info to the database as requests come in (and are sucessful or not.)
+    '''
+
+    def __init__(self):
+        self._conn = mdb.connect(user=settings.DB_ROOT_USERNAME, passwd=settings.DB_ROOT_PASSWORD)
+        self._setup()
+                
+        self._conn = mdb.connect(user=settings.DB_DBNAME, passwd=settings.DB_PASSWORD, db=settings.DB_DBNAME)
+
+    def _setup(self):
+        cur = self._conn.cursor()
+        
+        # Note that the DB name doesn't seem to be parameterizable.
+        
+        # We're going to pre-check for the DB and the table even though we're 
+        # using "IF NOT EXISTS", because otherwise it prints error text (which
+        # causes a problem when it's a cron job).
+        if not cur.execute('SHOW DATABASES') or (settings.DB_DBNAME,) not in cur.fetchall():
+            cur.execute('CREATE DATABASE IF NOT EXISTS '+settings.DB_DBNAME)
+            
+        # The GRANT command implictly creates the user if it doesn't exist.
+        cur.execute("GRANT ALL PRIVILEGES ON "+settings.DB_DBNAME+".* TO %s@'%%' IDENTIFIED BY %s WITH GRANT OPTION;", (settings.DB_USERNAME, settings.DB_PASSWORD,))
+        
+        cur.execute('USE '+settings.DB_DBNAME)
+       
+        if not cur.execute('SHOW TABLES IN '+settings.DB_DBNAME) or \
+                not set((('stats_per_addr',), 
+                         ('stats_exceptions',), 
+                         ('stats_noreply',))).issubset(set(cur.fetchall())):
+            cur.execute('CREATE TABLE IF NOT EXISTS stats_per_addr ( date DATE, addr VARCHAR(50) NOT NULL, count INT NOT NULL, PRIMARY KEY (date, addr) );')
+            cur.execute('CREATE TABLE IF NOT EXISTS stats_exceptions ( date DATE PRIMARY KEY, count INT NOT NULL );')
+            cur.execute('CREATE TABLE IF NOT EXISTS stats_noreply ( date DATE PRIMARY KEY, count INT NOT NULL );')
+            cur.execute('CREATE TABLE IF NOT EXISTS stats_blacklist ( date DATE PRIMARY KEY, count INT NOT NULL );')
+    
+    def increment_stats_per_addr(self, addr):
+        cur = self._conn.cursor()
+        cur.execute('INSERT INTO stats_per_addr (date, addr, count) VALUES (CURRENT_DATE(), %s, 1) ON DUPLICATE KEY UPDATE count = count+1', (addr,))
+
+    def increment_stats_exceptions(self):
+        cur = self._conn.cursor()
+        cur.execute('INSERT INTO stats_exceptions (date, count) VALUES (CURRENT_DATE(), 1) ON DUPLICATE KEY UPDATE count = count+1')
+
+    def increment_stats_noreply(self):
+        cur = self._conn.cursor()
+        cur.execute('INSERT INTO stats_noreply (date, count) VALUES (CURRENT_DATE(), 1) ON DUPLICATE KEY UPDATE count = count+1')
+
+    def increment_stats_blacklist(self):
+        cur = self._conn.cursor()
+        cur.execute('INSERT INTO stats_blacklist (date, count) VALUES (CURRENT_DATE(), 1) ON DUPLICATE KEY UPDATE count = count+1')
+        
+    def get_report(self):
+        '''
+        Extracts stats info from the database and generates a text report
+        '''
+        
+        cur = self._conn.cursor()
+        
+        # We'll report on the most recent full day. We'll determine this by 
+        # taking the second-most-recent date from the stats_per_addr table
+        # (which surely is getting updated every day).
+        if not cur.execute('SELECT date FROM stats_per_addr GROUP BY DATE ORDER BY DATE DESC LIMIT 1 OFFSET 1;'):
+            # We have no results yet.
+            return '(no data)'
+        
+        date = cur.fetchall()[0][0]
+                
+        report = 'Date: %s\n\n' % date
+        
+        # These tables all have only a single row with a single (non-date) value 
+        # for the most recent stats.
+        for table in ('stats_exceptions', 'stats_noreply', 'stats_blacklist'):
+            if cur.execute('SELECT count FROM '+table+' WHERE date = %s;', date):
+                res = cur.fetchall()[0]
+                report += '%s: %d\n' % (table, res[0])
+        
+        report += '\n'
+                
+        # The stats-per-addr table has multiple entries per day (zero or one
+        # for each email address we're serving), so it'll need a more custom
+        # report format.
+        
+        num_res = cur.execute('''
+            SELECT addr, count FROM stats_per_addr 
+            WHERE date = %s''', date)
+        res = cur.fetchall()
+        
+        report += 'Responses sent by address:\n'
+        
+        if not num_res:
+            report += '(none)'
+            
+        for row in res:
+            report += '%s: %d\n' % row
+            
+        return report
+
+
+if __name__ == '__main__':
+
+    # Open the connection. Uses creds from boto conf or env vars.
+    conn = SESConnection()
+
+    quota = conn.get_send_quota()
+    
+    # Getting an error when we try to call this. See:
+    # http://code.google.com/p/boto/issues/detail?id=518
+    #conn.close()
+    
+    email_body = ''
+    email_body += json.dumps(quota, indent=2)
+    email_body += '\n\n'
+
+    subject = '[MailResponder] Stats'
+
+    raw_email = sendmail.create_raw_email(settings.STATS_RECIPIENT_ADDRESS, 
+                                          settings.STATS_SENDER_ADDRESS, 
+                                          subject, 
+                                          email_body)
+
+    if not raw_email:
+        exit(1)
+
+    if not sendmail.send_raw_email_amazonses(raw_email, settings.STATS_SENDER_ADDRESS):
+        exit(1)
+
+    exit(0)
+
