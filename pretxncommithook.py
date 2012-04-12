8f22f982e0f7ea8806e943833a881768dc226367
Adam Pritchard <pritchard.adam@gmail.com>
2012-04-12 15:07:39 -0400

    Added comment for using the hook

--- pretxncommithook.py
+++ pretxncommithook.py
@@ -17,6 +17,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+# To use, add these lines to your .hg/hgrc file:
+#  [hooks]
+#  pretxncommit = python pretxncommithook.py
+
+
 import sys
 import os
 
