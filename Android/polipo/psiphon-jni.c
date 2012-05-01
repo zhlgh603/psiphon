6bc1ae81fcf70ac835811f7d900d1fd7fc235f5a
Rod Hynes <r.hynes@psiphon.ca>
2012-05-01 12:32:52 -0400

    Fixed string; input/output params for Polipo JNI; start Polipo thread

---
+++ Android/polipo/psiphon-jni.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2012, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "jni.h"
+
+int psiphonMain(int proxyPort, int localParentProxyPort);
+
+JNIEXPORT jint JNICALL Java_com_psiphon3_Polipo_runPolipo(
+    JNIEnv* env,
+    jobject obj,
+    int proxyPort,
+    int localParentProxyPort)
+{
+    // TODO: pass in a "stop" object and have polipo gracefully shutdown when the stop flag is set
+
+    return psiphonMain(proxyPort, localParentProxyPort);
+}
