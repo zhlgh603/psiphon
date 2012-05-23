f442b56c477725f7175967760e5021051cee0641
Adam Pritchard <pritchard.adam@gmail.com>
2012-05-23 15:26:45 -0400

    Fixed bug: Windows client would crash if Polipo couldn't start successfully. (Transport wasn't being cleaned up properly.)

---
+++ Client/psiclient/usersettings.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2011, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "stdafx.h"
+#include "usersettings.h"
+#include "config.h"
+#include "utilities.h"
+
+
+void InitializeUserSettings(void)
+{
+    // Read - and consequently write out default values for - all settings
+    UserSkipBrowser();
+    UserSkipProxySettings();
+}
+
+
+bool GetUserSetting(const string& settingName)
+{
+    DWORD value = 0;
+
+    if (!ReadRegistryDwordValue(settingName, value))
+    {
+        // Write out the setting with a "false" settingValue so that it's there
+        // for users to see and use, if they want to set it.
+        WriteRegistryDwordValue(settingName, 0);
+    }
+
+    return value == 1;
+}
+
+
+bool UserSkipBrowser(void)
+{
+    return GetUserSetting(LOCAL_SETTINGS_REGISTRY_VALUE_USER_SKIP_BROWSER);
+}
+
+
+bool UserSkipProxySettings(void)
+{
+    return GetUserSetting(LOCAL_SETTINGS_REGISTRY_VALUE_USER_SKIP_PROXY_SETTINGS);
+}
