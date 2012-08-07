8e6be67e3d694277cf5ddfcd95ae86f47521a5b1
Rod Hynes <r.hynes@psiphon.ca>
2012-08-07 15:10:06 -0400

    Enable cherrypy stats fetching

--- Server/psi_web.py
+++ Server/psi_web.py
@@ -37,6 +37,7 @@ import json
 import re
 from cherrypy import wsgiserver, HTTPError
 from cherrypy.wsgiserver import ssl_pyopenssl
+from cherrypy.lib import cpstats
 from webob import Request
 import psi_psk
 import psi_config
@@ -492,6 +493,22 @@ class ServerInstance(object):
         start_response('200 OK', [])
         return []
 
+    def stats(self, environ, start_response):
+        # Just check the server secret; no logging or action for this request
+        request = Request(environ)
+        if ('server_secret' not in request.params or
+            not constant_time_compare(request.params['server_secret'], self.server_secret) or
+            'stats_client_secret' not in request.params or
+            not hasattr(psi_config, 'STATS_CLIENT_SECRET')
+            not constant_time_compare(request.params['stats_client_secret'], psi_config.STATS_CLIENT_SECRET)):
+            start_response('404 Not Found', [])
+            return []
+        contents = ''.join(list(cpstats.StatsPage().index()))
+        response_headers = [('Content-Type', 'text/html'),
+                            ('Content-Length', '%d' % (len(contents),))]
+        start_response('200 OK', response_headers)
+        return [contents]
+
 
 def get_servers():
     # enumerate all interfaces with an IPv4 address and server entry
@@ -568,8 +585,11 @@ class WebServerThread(threading.Thread):
                                      '/status': server_instance.status,
                                      '/speed': server_instance.speed,
                                      '/feedback': server_instance.feedback,
-                                     '/check': server_instance.check}),
+                                     '/check': server_instance.check,
+                                     '/stats': server_instance.stats}),
                                 numthreads=self.server_threads, timeout=20)
+
+                self.server.stats['Enabled'] = True
 
                 # Set maximum request input sizes to avoid processing DoS inputs
                 self.server.max_request_header_size = 100000
