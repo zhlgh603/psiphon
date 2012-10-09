486b27b6f3bc811c851bc234070eb5f278cec7c6
Adam Pritchard <a.pritchard@psiphon.ca>
2012-10-09 14:24:06 -0400

    Removed use-only-embedded-servers change. Can be achieved using IGNORE_SYSTEM_SERVER_LIST value in embeddedvalues.h

--- Client/psiclient/serverlist.cpp
+++ Client/psiclient/serverlist.cpp
@@ -167,11 +167,6 @@ ServerEntry ServerList::GetNextServer()
 
     ServerEntries serverEntryList = GetList();
 
-#if defined TESTING && defined _DEBUG
-	// Disable load-balancing so that we can always use the test server
-	serverEntryList = GetListFromEmbeddedValues();
-#endif
-
     if (serverEntryList.size() < 1)
     {
         throw std::exception("No servers found.  This application is possibly corrupt.");
