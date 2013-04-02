2479b1ef4e920c2954600ac99d2503b560c04725
rhynes <rhynes@localhost>
2013-04-02 16:06:28 -0400

    Merge

---
+++ Client/psiclient/Resource.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2011, Psiphon Inc.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef IDC_STATIC
+#define IDC_STATIC                          -1
+#endif
+
+#define IDD_PSICLIENT_DIALOG                101
+#define IDS_APP_TITLE                       102
+#define IDD_ABOUTBOX                        103
+#define IDM_ABOUT                           104
+#define IDM_EXIT                            105
+#define IDI_PSICLIENT                       107
+#define IDC_PSICLIENT                       109
+#define IDM_TOGGLE                          110
+#define IDM_SHOW_DEBUG_MESSAGES             112
+#define IDM_HELP                            113
+#define IDR_MAINFRAME                       128
+#define IDT_BUTTON_ANIMATION                129
+#define IDB_TOGGLE_BUTTON_IMAGES            200
+#define IDB_BANNER                          201
+#define IDB_EMAIL                           202
+#define IDB_FEEDBACK_BUTTON_IMAGES          203
+#define IDR_PLONK_EXE                       500
+#define IDR_POLIPO_EXE                      501
+#define IDC_TOGGLE_BUTTON                   600
+#define IDC_BANNER_STATIC                   601
+#define IDC_LOG_LISTBOX                     602
+#define IDC_INFO_LINK_STATIC                603
+#define IDC_SPLIT_TUNNEL_CHECKBOX           604
+#define IDC_FEEDBACK_BUTTON                 605
+#define IDC_TRANSPORT_OPTION_RADIO_FIRST    700
+// 701-798 are reserved
+#define IDC_TRANSPORT_OPTION_RADIO_LAST     799
