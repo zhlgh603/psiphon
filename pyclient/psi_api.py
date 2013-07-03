a289ea6e77a0ded0109a7d5dfd725343000c838b
Adam Pritchard <a.pritchard@psiphon.ca>
2013-07-03 17:57:30 -0400

    Creating task branch

---
+++ pyclient/psi_api.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2012, Psiphon Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+import urllib2
+import httplib
+import ssl
+import socket
+import binascii
+
+
+#
+# Psiphon 3 Server API
+#
+
+class Psiphon3Server(object):
+
+    def __init__(self, servers, propagation_channel_id, sponsor_id, client_version):
+        self.servers = servers
+        # TODO: read the new json config element of the server entry, if present
+        (self.ip_address, self.web_server_port, self.web_server_secret,
+         self.web_server_certificate) = binascii.unhexlify(servers[0]).split(" ")[:4]
+        self.propagation_channel_id = propagation_channel_id
+        self.sponsor_id = sponsor_id
+        self.client_version = client_version
+        # TODO: add proxy support
+        handler = CertificateMatchingHTTPSHandler(self.web_server_certificate)
+        self.opener = urllib2.build_opener(handler)
+
+    # handshake
+    # Note that self.servers may be updated with newly discovered servers after a successful handshake
+    def handshake(self, relay_protocol):
+        # TODO: page view regexes
+        request_url = (self._common_request_url(relay_protocol) % ('handshake',) + '&' +
+                       '&'.join(['known_server=%s' % (binascii.unhexlify(server).split(" ")[0],) for server in self.servers]))
+        response = self.opener.open(request_url).read()
+        handshake_response = {'Upgrade': '',
+                              'SSHPort': '',
+                              'SSHUsername': '',
+                              'SSHPassword': '',
+                              'SSHHostKey': '',
+                              'SSHSessionID': '',
+                              'SSHObfuscatedPort': '',
+                              'SSHObfuscatedKey': '',
+                              'PSK': '',
+                              'Homepage': []}
+
+        for line in response.split('\n'):
+            key, value = line.split(': ', 1)
+            if key in handshake_response.keys():
+                if type(handshake_response[key]) == list:
+                    handshake_response[key].append(value)
+                else:
+                    handshake_response[key] = value
+            if key == 'Server':
+                # discovery
+                if value not in self.servers:
+                    self.servers.insert(1, value)
+            if key == 'SSHSessionID':
+                self.ssh_session_id = value
+
+        return handshake_response
+
+    # TODO: download
+
+    # connected
+    # For SSH and OSSH, SSHSessionID from the handshake response is used when session_id is None
+    # For VPN, the VPN IP Address should be used for session_id (ie. 10.0.0.2)
+    def connected(self, relay_protocol, session_id=None):
+        if not session_id and relay_protocol in ['SSH', 'OSSH']:
+            session_id = self.ssh_session_id
+        assert session_id is not None
+
+        request_url = (self._common_request_url(relay_protocol) % ('connected',) +
+                       '&session_id=%s' % (session_id,))
+        self.opener.open(request_url)
+
+    # disconnected
+    # For SSH and OSSH, SSHSessionID from the handshake response is used when session_id is None
+    # For VPN, this should not be called
+    def disconnected(self, relay_protocol, session_id=None):
+        assert relay_protocol not in ['VPN']
+        if not session_id and relay_protocol in ['SSH', 'OSSH']:
+            session_id = self.ssh_session_id
+        assert session_id is not None
+
+        request_url = (self._common_request_url(relay_protocol) % ('status',) +
+                       '&session_id=%s&connected=%s' % (session_id, '0'))
+        self.opener.open(request_url)
+
+    # TODO: failed
+
+    # TODO: status
+
+    def _common_request_url(self, relay_protocol):
+        assert relay_protocol in ['VPN','SSH','OSSH']
+        return 'https://%s:%s/%%s?server_secret=%s&propagation_channel_id=%s&sponsor_id=%s&client_version=%s&relay_protocol=%s' % (
+            self.ip_address, self.web_server_port, self.web_server_secret,
+            self.propagation_channel_id, self.sponsor_id, self.client_version,
+            relay_protocol)
+
+
+#
+# CertificateMatchingHTTPSHandler
+#
+# Adapted from CertValidatingHTTPSConnection and VerifiedHTTPSHandler
+# http://stackoverflow.com/questions/1087227/validate-ssl-certificates-with-python
+#
+
+class InvalidCertificateException(httplib.HTTPException, urllib2.URLError):
+
+    def __init__(self, host, cert, reason):
+        httplib.HTTPException.__init__(self)
+        self.host = host
+        self.cert = cert
+        self.reason = reason
+
+    def __str__(self):
+        return ('Host %s returned an invalid certificate (%s) %s\n' %
+                (self.host, self.reason, self.cert))
+
+
+class CertificateMatchingHTTPSConnection(httplib.HTTPConnection):
+
+    def __init__(self, host, expected_server_certificate, **kwargs):
+        httplib.HTTPConnection.__init__(self, host, **kwargs)
+        self.expected_server_certificate = expected_server_certificate
+
+    def connect(self):
+        sock = socket.create_connection((self.host, self.port))
+        self.sock = ssl.wrap_socket(sock)
+        cert = ssl.DER_cert_to_PEM_cert(self.sock.getpeercert(True))
+        # Remove newlines and -----BEGIN CERTIFICATE----- and -----END CERTIFICATE-----
+        cert = ''.join(cert.splitlines())[len('-----BEGIN CERTIFICATE-----'):-len('-----END CERTIFICATE-----')]
+        if cert != self.expected_server_certificate:
+            raise InvalidCertificateException(self.host, cert,
+                                              'server presented the wrong certificate')
+
+
+class CertificateMatchingHTTPSHandler(urllib2.HTTPSHandler):
+
+    def __init__(self, expected_server_certificate):
+        urllib2.AbstractHTTPHandler.__init__(self)
+        self.expected_server_certificate = expected_server_certificate
+
+    def https_open(self, req):
+        def http_class_wrapper(host, **kwargs):
+            return CertificateMatchingHTTPSConnection(
+                    host, self.expected_server_certificate, **kwargs)
+
+        try:
+            return self.do_open(http_class_wrapper, req)
+        except urllib2.URLError, e:
+            if type(e.reason) == ssl.SSLError and e.reason.args[0] == 1:
+                raise InvalidCertificateException(req.host, '',
+                                                  e.reason.args[1])
+            raise
+
+    https_request = urllib2.HTTPSHandler.do_request_ 
+
+
