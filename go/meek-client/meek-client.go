9457ee087296ea9302fe31a86c2dc9ba91380a40
e.fryntov <e.fryntov@psiphon.ca>
2014-05-08 13:01:40 -0400

    Meek client takes arguments via SOCK4A interface

--- go/meek-client/meek-client.go
+++ go/meek-client/meek-client.go
@@ -2,81 +2,67 @@ package main
 
 import (
 	"bitbucket.org/psiphon/psiphon-circumvention-system/go/utils/crypto"
+	"bufio"
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
-	"os"
 	"time"
 )
 
-const sessionIdLength = 32
-const maxPayloadLength = 0x10000
-const initPollInterval = 100 * time.Millisecond
-const maxPollInterval = 5 * time.Second
-const pollIntervalMultiplier = 1.5
+import "git.torproject.org/pluggable-transports/goptlib.git"
 
-var gCookiePayload string //base64 encoded encrypted cookie payload
+const (
+	// The size of the largest chunk of data we will read from the SOCKS
+	// port before forwarding it in a request, and the maximum size of a
+	// body we are willing to handle in a reply.
+	maxPayloadLength = 0x10000
+	// We must poll the server to see if it has anything to send; there is
+	// no way for the server to push data back to us until we send an HTTP
+	// request. When a timer expires, we send a request even if it has an
+	// empty body. The interval starts at this value and then grows.
+	initPollInterval = 100 * time.Millisecond
+	// Maximum polling interval.
+	maxPollInterval = 5 * time.Second
+	// Geometric increase in the polling interval each time we fail to read
+	// data.
+	pollIntervalMultiplier = 1.5
 
-var gConfig struct {
+	methodName = "meek"
+)
+
+// RequestInfo encapsulates all the configuration used for a request–response
+// roundtrip, including variables that may come from SOCKS args or from the
+// command line.
+type RequestInfo struct {
 	ClientPublicKeyBase64 string
 	ObfuscationKeyword    string
 	MeekServerAddr        string
 	PsiphonServerAddr     string
-	MeekRelayAddr         string
 	FrontingDomain        string
 	FrontingHostname      string
 	SshSessionID          string
+	CookiePayload         string
 }
 
-func makeCookie() (string, error) {
-	var clientPublicKey, dummyKey [32]byte
-
-	keydata, err := base64.StdEncoding.DecodeString(gConfig.ClientPublicKeyBase64)
-	if err != nil {
-		return "", fmt.Errorf("error decoding gConfig.ClientPublicKeyBase64: %s", err)
-	}
-
-	copy(clientPublicKey[:], keydata)
-	cr := crypto.New(clientPublicKey, dummyKey)
-
-	cookie := make(map[string]string)
-	cookie["m"] = gConfig.MeekServerAddr
-	cookie["p"] = gConfig.PsiphonServerAddr
-	cookie["s"] = gConfig.SshSessionID
-
-	j, err := json.Marshal(cookie)
-	if err != nil {
-		return "", err
-	}
-	encrypted, err := cr.Encrypt(j)
-	if err != nil {
-		return "", err
-	}
-	obfuscated, err := cr.Obfuscate(encrypted, gConfig.ObfuscationKeyword)
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(obfuscated), nil
-}
-
-func roundTrip(buf []byte) (*http.Response, error) {
+// Do an HTTP roundtrip using the payload data in buf and the request metadata
+// in info.
+func roundTrip(buf []byte, info *RequestInfo) (*http.Response, error) {
 	tr := http.DefaultTransport
 
 	var URL string
 	bFronting := false
 
-	if gConfig.FrontingDomain != "" {
+	if info.FrontingDomain != "" {
 		bFronting = true
-		URL = fmt.Sprintf("https://%s/", gConfig.FrontingDomain)
+		URL = fmt.Sprintf("https://%s/", info.FrontingDomain)
 	} else {
-		URL = fmt.Sprintf("https://%s/", gConfig.MeekRelayAddr)
+		URL = fmt.Sprintf("http://%s/", info.MeekServerAddr)
 	}
 
 	req, err := http.NewRequest("POST", URL, bytes.NewReader(buf))
@@ -84,17 +70,19 @@ func roundTrip(buf []byte) (*http.Response, error) {
 		return nil, err
 	}
 	if bFronting {
-		req.Host = gConfig.FrontingHostname
+		req.Host = info.FrontingHostname
 	}
 	req.Header.Set("Content-Type", "application/octet-stream")
 
-	cookie := &http.Cookie{Name: "key", Value: gCookiePayload}
+	cookie := &http.Cookie{Name: "key", Value: info.CookiePayload}
 	req.AddCookie(cookie)
 	return tr.RoundTrip(req)
 }
 
-func sendRecv(buf []byte, conn net.Conn) (int64, error) {
-	resp, err := roundTrip(buf)
+// Send the data in buf to the remote URL, wait for a reply, and feed the reply
+// body back into conn.
+func sendRecv(buf []byte, conn net.Conn, info *RequestInfo) (int64, error) {
+	resp, err := roundTrip(buf, info)
 	if err != nil {
 		return 0, err
 	}
@@ -107,29 +95,72 @@ func sendRecv(buf []byte, conn net.Conn) (int64, error) {
 	return io.Copy(conn, io.LimitReader(resp.Body, maxPayloadLength))
 }
 
-func pollingLoop(conn net.Conn) error {
-	buf := make([]byte, maxPayloadLength)
+// Repeatedly read from conn, issue HTTP requests, and write the responses back
+// to conn.
+func copyLoop(conn net.Conn, info *RequestInfo) error {
 	var interval time.Duration
 
-	interval = initPollInterval
-	for {
-		conn.SetReadDeadline(time.Now().Add(interval))
-		nr, readErr := conn.Read(buf)
+	ch := make(chan []byte)
 
-		nw, err := sendRecv(buf[:nr], conn)
+	// Read from the Conn and send byte slices on the channel.
+	go func() {
+		var buf [maxPayloadLength]byte
+		r := bufio.NewReader(conn)
+		for {
+			n, err := r.Read(buf[:])
+			b := make([]byte, n)
+			copy(b, buf[:n])
+			// log.Printf("read from local: %q", b)
+			ch <- b
+			if err != nil {
+				log.Printf("error reading from local: %s", err)
+				break
+			}
+		}
+		close(ch)
+	}()
+
+	interval = initPollInterval
+loop:
+	for {
+		var buf []byte
+		var ok bool
+
+		// log.Printf("waiting up to %.2f s", interval.Seconds())
+		// start := time.Now()
+		select {
+		case buf, ok = <-ch:
+			if !ok {
+				break loop
+			}
+			// log.Printf("read %d bytes from local after %.2f s", len(buf), time.Since(start).Seconds())
+		case <-time.After(interval):
+			// log.Printf("read nothing from local after %.2f s", time.Since(start).Seconds())
+			buf = nil
+		}
+
+		nw, err := sendRecv(buf, conn, info)
 		if err != nil {
 			return err
 		}
-
-		if readErr != nil {
-			if e, ok := readErr.(net.Error); !ok || !e.Timeout() {
-				return readErr
+		/*
+			if nw > 0 {
+				log.Printf("got %d bytes from remote", nw)
+			} else {
+				log.Printf("got nothing from remote")
 			}
-		}
+		*/
 
-		if nw > 0 {
+		if nw > 0 || len(buf) > 0 {
+			// If we sent or received anything, poll again
+			// immediately.
+			interval = 0
+		} else if interval == 0 {
+			// The first time we don't send or receive anything,
+			// wait a while.
 			interval = initPollInterval
 		} else {
+			// After that, wait a little longer.
 			interval = time.Duration(float64(interval) * pollIntervalMultiplier)
 		}
 		if interval > maxPollInterval {
@@ -140,19 +171,86 @@ func pollingLoop(conn net.Conn) error {
 	return nil
 }
 
-func acceptLoop(ln net.Listener) error {
+func makeCookie(info *RequestInfo) (string, error) {
+	var clientPublicKey, dummyKey [32]byte
+
+	keydata, err := base64.StdEncoding.DecodeString(info.ClientPublicKeyBase64)
+	if err != nil {
+		return "", fmt.Errorf("error decoding info.ClientPublicKeyBase64: %s", err)
+	}
+
+	copy(clientPublicKey[:], keydata)
+	cr := crypto.New(clientPublicKey, dummyKey)
+
+	cookie := make(map[string]string)
+	cookie["p"] = info.PsiphonServerAddr
+	cookie["s"] = info.SshSessionID
+
+	j, err := json.Marshal(cookie)
+	if err != nil {
+		return "", err
+	}
+	encrypted, err := cr.Encrypt(j)
+	if err != nil {
+		return "", err
+	}
+	obfuscated, err := cr.Obfuscate(encrypted, info.ObfuscationKeyword)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(obfuscated), nil
+}
+
+// Callback for new SOCKS requests.
+func handler(conn *pt.SocksConn) error {
+	defer conn.Close()
+	err := conn.Grant(&net.TCPAddr{IP: net.ParseIP("0.0.0.0"), Port: 0})
+	if err != nil {
+		return err
+	}
+
+	var info RequestInfo
+
+	socksJSON := []byte(conn.Req.Username)
+	err = json.Unmarshal(socksJSON, &info)
+	if err != nil {
+		return errors.New(fmt.Sprintf("couldn't decode SOCKS JSON payload: %s", err.Error()))
+	}
+
+	if info.ClientPublicKeyBase64 == "" {
+		return errors.New("ClientPublicKeyBase64 is missing from SOCKS JSON payload")
+	}
+	if info.PsiphonServerAddr == "" {
+		return errors.New("ClientPublicKeyBase64 is missing from SOCKS JSON payload")
+	}
+
+	if info.MeekServerAddr == "" && info.FrontingDomain == "" {
+		return errors.New("Both MeekServerAddr & FrontingDomain are missing from SOCKS JSON payload")
+	}
+	if info.SshSessionID == "" {
+		return errors.New("SshSessionID is missing from SOCKS JSON payload")
+	}
+	info.CookiePayload, err = makeCookie(&info)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Couldn't create encrypted payload: %s", err.Error()))
+	}
+
+	return copyLoop(conn, &info)
+}
+
+func acceptLoop(ln *pt.SocksListener) error {
 	defer ln.Close()
 	for {
-		conn, err := ln.Accept()
+		conn, err := ln.AcceptSocks()
 		if err != nil {
-			log.Printf("error in Accept: %s", err)
+			log.Printf("error in AcceptSocks: %s", err)
 			if e, ok := err.(net.Error); ok && !e.Temporary() {
 				return err
 			}
 			continue
 		}
 		go func() {
-			err := pollingLoop(conn)
+			err := handler(conn)
 			if err != nil {
 				log.Printf("error in handling request: %s", err)
 			}
@@ -163,44 +261,12 @@ func acceptLoop(ln net.Listener) error {
 
 func main() {
 	var err error
-	flag.StringVar(&gConfig.ClientPublicKeyBase64, "cpubkey", "", "Client public key required to encrypt cookie payload")
-	flag.StringVar(&gConfig.ObfuscationKeyword, "obfskeyword", "", "Obfuscation keyword")
-	flag.StringVar(&gConfig.MeekServerAddr, "meekserver", "", "Meek server address(x.x.x.x:n")
-	flag.StringVar(&gConfig.PsiphonServerAddr, "psiphonserver", "", "Psiphon server address(x.x.x.x:n")
-	flag.StringVar(&gConfig.MeekRelayAddr, "meekrelay", "", "Meek relay address(x.x.x.x:n")
-	flag.StringVar(&gConfig.FrontingDomain, "frontdomain", "", "Domain to use for fronting")
-	flag.StringVar(&gConfig.FrontingHostname, "fronthost", "", "Host header to use for fronting")
-	flag.StringVar(&gConfig.SshSessionID, "sessid", "", "Client SSH session id")
-	flag.Parse()
 
-	if gConfig.ClientPublicKeyBase64 == "" {
-		log.Fatalf("-cpubkey is a required argument, exiting now")
-	}
-	if gConfig.MeekServerAddr == "" {
-		log.Fatalf("-meekserver is a required argument, exiting now")
-	}
-	if gConfig.PsiphonServerAddr == "" {
-		log.Fatalf("-psiphonserver is a required argument, exiting now")
-	}
-	if gConfig.SshSessionID == "" {
-		log.Fatalf("-sessid is a required argument, exiting now")
-	}
-	if gConfig.MeekRelayAddr == "" && gConfig.FrontingDomain == "" {
-		log.Fatalf("Either -meekrelay or -frontdomain has to be specified, exiting now")
-	}
-	if gConfig.FrontingDomain != "" && gConfig.FrontingHostname == "" {
-		log.Fatalf("-fronthost is required with -frontdomain, exiting now")
-	}
-
-	gCookiePayload, err = makeCookie()
+	ln, err := pt.ListenSocks("tcp", "127.0.0.1:0")
 	if err != nil {
-		log.Fatalf("Couldn't create encrypted payload: %s", err.Error())
+		pt.CmethodError(methodName, err.Error())
+		return
 	}
-
-	ln, err := net.Listen("tcp", "127.0.0.1:0")
-	if err != nil {
-		log.Fatalf("Couldn't start a server: %s", err.Error())
-	}
-	fmt.Fprintln(os.Stdout, "CMETHOD meek", ln.Addr())
+	pt.Cmethod(methodName, ln.Version(), ln.Addr())
 	acceptLoop(ln)
 }
