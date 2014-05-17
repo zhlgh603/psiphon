bf61b6df675ff2eac5a61985f41ddb11de78d70e
Kruger <Kruger@localhost>
2014-05-16 21:51:02 -0400

    Backwards compatibility with older versions of redis.

--- Server/psi_auth.py
+++ Server/psi_auth.py
@@ -125,8 +125,9 @@ def set_session_region(pam_rhost, session_id):
             port=psi_config.SESSION_DB_PORT,
             db=psi_config.SESSION_DB_INDEX)
 
-    redis_session.setnx(session_id, json.dumps(geoip))
-    redis_session.expire(session_id, psi_config.SESSION_EXPIRE_SECONDS)
+    if redis_session.get(session_id) == None:
+        redis_session.set(session_id, json.dumps(geoip))
+        redis_session.expire(session_id, psi_config.SESSION_EXPIRE_SECONDS)
 
     # Now fill in the discovery database
     # NOTE: We are storing a value derived from the user's IP address
@@ -141,8 +142,9 @@ def set_session_region(pam_rhost, session_id):
                 port=psi_config.DISCOVERY_DB_PORT,
                 db=psi_config.DISCOVERY_DB_INDEX)
 
-        redis_discovery.setnx(session_id, json.dumps({'client_ip_address_strategy_value' : client_ip_address_strategy_value}))
-        redis_discovery.expire(session_id, psi_config.DISCOVERY_EXPIRE_SECONDS)
+        if redis_discovery.get(session_id) == None:
+            redis_discovery.set(session_id, json.dumps({'client_ip_address_strategy_value' : client_ip_address_strategy_value}))
+            redis_discovery.expire(session_id, psi_config.DISCOVERY_EXPIRE_SECONDS)
     except socket.error:
         pass
 
